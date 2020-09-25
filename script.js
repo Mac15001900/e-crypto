b6e2a7ebe1185e96b85807fb18dcd4e7f62167e3
Mac15001900 <mac15001900@gmail.com>
2020-09-25 02:05:45 +0200

    Added more buttons and field, some basic handling for then.

--- script.js
+++ script.js
@@ -1,37 +1,47 @@
 let myName = '';
+let team = '';
+let code = [];
+let members = [];
+//let gameState = {received = false, teams={}, codeDrawn=false, currentTeam=''};
+let gameState = {'received': false};
 
-const CHANNEL_ID = 'M0aA6Z7AxYWcGGt3';
+const NUMER_OF_WORDS = 4;
+
+const CHANNEL_ID = '5WQg2mc3UkqAxomd';
 const drone = new ScaleDrone(CHANNEL_ID, {
   data: { // Will be sent out as clientData via events
     name: getUsername(),
-    color: getRandomColor(),
+    color: "#0x000000",
   },
 });
 const DOM = {
-  membersCount: document.querySelector('.members-count'),
-  membersList: document.querySelector('.members-list'),
-  messages: document.querySelector('.messages-day'),
-  input: document.querySelector('.message-form_main_input'),
-  input2: document.querySelector('.message-form_secondary_input'),
-  form: document.querySelector('.message-form'),
+  membersCount: document.querySelector('#members-count'),
+  membersList: document.querySelector('#members-list'),
+  messages: document.querySelector('#messages'),
+  input: document.querySelector('#text-input'),
+  form: document.querySelector('#form'),
+  codeButton: document.querySelector('#codeButton'),
+  redButton: document.querySelector('#redButton'),
+  blueButton: document.querySelector('#blueButton')
 };
 
-let members = [];
 
+//Test
+DOM.input.disabled = false;
 
 //Name & color generation
 function getRandomName() {
   const adjs = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
   const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
   const name = adjs[Math.floor(Math.random() * adjs.length)] +
-   "_" +
-   nouns[Math.floor(Math.random() * nouns.length)];
+   "_" + nouns[Math.floor(Math.random() * nouns.length)];
   myName = name;
   return (name);
 }
 
 function getUsername() {
-  var name = prompt("Enter your username","");
+  //var name = prompt("Enter your username","");
+  name = getRandomName();
   while(!name){
     var name = prompt("Enter your username (it can't be empty)","");
   }
@@ -41,6 +51,24 @@ function getUsername() {
  
 function getRandomColor() {
  return '#' + Math.floor(Math.random() * 0.8 * 0xFFFFFF).toString(16);
+}
+
+function pickTeam(members) {
+  var reds = 0, blues = 0;
+  for (var i = 0; i < members.length; i++) {
+    let memberTeam = members[i].clientData.team;
+    gameState.teams[members[i].clientData.name] = memberTeam;
+    if(memberTeam === 'red'){
+      reds++
+    }else if(memberTeam === 'blue'){
+      blues++
+    }    
+  }
+  if(reds<=blues){
+    return('red');
+  }else{
+    return('blue');
+  }
 }
 
 
@@ -64,40 +92,94 @@ function updateMembersDOM() {
   );
 }
  
-function createMessageElement(text, member) {
-  const el = document.createElement('div');
-  el.appendChild(createMemberElement(member));
-  el.appendChild(document.createTextNode(text));
-  el.className = 'message';
-  return el;
-}
- 
-function addMessageToListDOM(text, member) {
+function addElementToListDOM(element) {
   const el = DOM.messages;
   const wasTop = el.scrollTop === el.scrollHeight - el.clientHeight;
-  el.appendChild(createMessageElement(text, member));
+  el.appendChild(element);
   if (wasTop) {
    el.scrollTop = el.scrollHeight - el.clientHeight;
   }
 }
+ 
+function addMessageToListDOM(text, member) {
+  const el = document.createElement('div');
+  if(member) el.appendChild(createMemberElement(member));
+  el.appendChild(document.createTextNode(text));
+  el.className = 'message';
+  
+  addElementToListDOM(el);
+}
+
+//Getting codes
+codeButton.addEventListener("click", function () {
+  if(code.length === 0){
+    randomiseCode();
+    addMessageToListDOM('--Kod to: '+code+"--");
+    codeButton.text = 'Ujawnij kod';
+  } else{
+    sendMessage('code', code);
+    codeButton.text = 'Dobierz kod';
+    code = [];
+  }
+  
+})
+
+function randomiseCode() {
+  let codeElements = [];
+  let elementsLeft = [1,2,3,4];
+  while(codeElements.length < NUMER_OF_WORDS - 1){
+    let index = Math.floor(Math.random() * elementsLeft.length);
+    codeElements.push(elementsLeft[index]);
+    elementsLeft.splice(index,1);
+  }
+  code = codeElements;
+  return(codeElements);
+}
+
+//Switching team
+redButton.addEventListener("click", function () {
+  team = 'R';
+  redButton.style  = "visibility : hidden";
+  blueButton.style = "visibility : visible";
+  codeButton.style = "visibility : visible";
+  //TODO update words
+})
+
+blueButton.addEventListener("click", function () {
+  team = 'B';
+  redButton.style  = "visibility : visible";
+  blueButton.style = "visibility : hidden";
+  codeButton.style = "visibility : visible";
+  //TODO update words
+})
+
 
 //Sending messages
-DOM.form.addEventListener('submit', sendMessage);
- 
-function sendMessage() {
+DOM.form.addEventListener('submit', sendFormMessage);
+
+function sendFormMessage() {
   const content = DOM.input.value;
-  const recipient = DOM.input2.value;
+  DOM.input.value = '';
+  if(code.length > 0){
+    sendMessage('hint',content)  
+  } else {
+    sendMessage('general',content)
+  }
+  
+}
+
+function sendMessage(type, content) {
   if (content === '') {
     return;
   }
-  DOM.input.value = '';
+  
   drone.publish({
     room: 'observable-room',
     message: {
-      text: content,
-      recipient: recipient
+      type: type,
+      content: content
     },
-  });
+  }); 
 }
 
 //Room connection
@@ -118,12 +200,22 @@ drone.on('open', error => {
 	 // List of currently online members, emitted once
 	room.on('members', m => {
   	members = m;
+    if(members.length === 1) {
+      //First to enter the room; initialise the game
+      team = 'red';
+      gameState.teams[myName] = 'red';
+      gameState.currentTeam = Math.random()<0.5 ? 'red' : 'blue';
+      gameState.received = true;
+    }
   	updateMembersDOM(); 
 	});
 	 
 	// User joined the room
 	room.on('member_join', member => {
 	  members.push(member);
+    if(gameState.received){
+      sendMessage('welcome', gameState);  
+    }    
 	  updateMembersDOM();
 	});
 	 
@@ -136,11 +228,31 @@ drone.on('open', error => {
 
 	room.on('data', (data, member) => {
 	  if (member) {
-      if(data.recipient !== '') {
-        if(data.recipient === myName || member.clientData.name === myName) addMessageToListDOM(data.text, member);
+      switch(data.type){
+        case 'general':
+          addMessageToListDOM(data.content, member); 
+          console.log(data);
+          break;
+        case 'hint':
+          addMessageToListDOM(data.content, member); 
+          console.log(data);
+          break;
+        case 'code':
+          addMessageToListDOM(data.content, member); 
+          console.log(data);
+          break;
+        case 'teamsUpdate': 
+          teams = data.content; //TODO update teams display
+          break;
+        case 'welcome':
+          if(!gameState.received){
+            gameState = data.content;
+          }
+          break;
+        default: alert('Unkown message type received: '+data.type)
 
       }
-	    else addMessageToListDOM(data.text, member);
+
 	  } else {
 	    // Message is from server
 	 }

8b720d4b21cf4785d0d010f0046253cb63e121c4
Mac15001900 <mac15001900@gmail.com>
2020-10-15 02:57:02 +0200

    Added RoundState and everything that comes with tracking it

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
+const RS = {
+  NO_GAME: 0, //There is no game going on
+  START: 1,  //Start of a subround, nothing happened yet
+  HINT_GIVEN: 2, //A hint has just been given (instantly skips to ENEMY_GUESSED if round 1)
+  ENEMY_GUESSED: 3, //The enemy team made a guess 
+  ALLY_GUESSED: 4, //The allied team has made a guess, waiting for the code reveal (this doesn't last long)
+  ROUND_END: 5 //Code was revealed, processing the end of the subround (this also doesn't last long)
+};
+
+//Team and player tracking
 let myName = '';
-let team = '';
-let nextTeam = '';
-let code = [];
-let forceGuessMode = false;
 let members = [];
+let team = '';
+let nextTeam = ''; //Team we'll switch to next game
+
+//Hint giver variables
+let isHintGiver = false; //True between giving out a hint and releaving the code.
+let code = [];
+let forceGuessMode = false; //UI variable
+let guesses = {'R':[],'B':[]};
+
+//Team variables
 let words = [];
 let rerollsLeft= 0;
-let gameState = {'received': false, 'memberData':[]};
+
+//Other
 let rulesShown = false;
 
+//gameState block:
+let gs = {'received': false, 'memberData':[], 'round':0, 'startingTeam':'','currentTeam':'','roundState':RS.NO_GAME,'hintGiver':{},'tokens':{}};
+
+//Game settings
 const NUMER_OF_WORDS = 4;
 const REROLLS_PER_GAME = 2;
+const NUMBER_OF_ROUNDS = 8;
+const TOKENS_NEEDED = 2;
+const ROOM_NAME = 'observable-room';
 const wordPool = [wordBank.en_basic,wordBank.en_pokemon_types,wordBank.en_fantasy].flat();
 const rerollWordPool = [wordBank.en_basic].flat();
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+//UI settings
 const NO_TEAM_COLOR = "#AAAAAA";
 const RED_TEAM_COLOR = "#F52020";
 const BLUE_TEAM_COLOR = "#3030F0";
 const RED_BACKGROUND = "#ed143d6b";
 const BLUE_BACKGROUND = '#00bfff5c';//'#469ae4';//"#00bfff5c"; #56affd
 
+//Network settings
 const CHANNEL_ID = '5WQg2mc3UkqAxomd';
 const drone = new ScaleDrone(CHANNEL_ID, {
   data: { // Will be sent out as clientData via events
@@ -28,6 +54,7 @@ const drone = new ScaleDrone(CHANNEL_ID, {
     color: "#000000",
   },
 });
+
 const DOM = {
   secretWordsDisplay: document.querySelector('#secretWordsDisplay'),
   membersList: document.querySelector('#members-list'),
@@ -83,6 +110,12 @@ function getMember(input) {
   return res;
 }
 
+function otherTeam(team) {
+  if(team === 'R') return 'B';
+  else if(team === 'B') return 'R';
+  else console.error('Invalid team '+ team);
+}
+
 
 
 //Handle displaying things
@@ -108,8 +141,7 @@ function updateMembersDOM() {
   //DOM.secretWordsDisplay.innerText = members.length+' '+s.player_count;
   DOM.membersList.innerHTML = '';
   members.sort(compareMembers).forEach(member =>
-   DOM.membersList.appendChild(createMemberElement(member))
-  );
+    DOM.membersList.appendChild(createMemberElement(member)));
 }
 
 function compareMembers(m1,m2) {
@@ -159,7 +191,7 @@ codeButton.addEventListener("click", function () {
       rulesShown = true;
     }
   } else{
-    sendMessage('codeReveal', code);
+    sendMessage('codeReveal', {'code':code});
     codeButton.text = s.draw_code;
     updateDescriptions(true);
     code = [];
@@ -261,11 +293,11 @@ function updateTeamStyle(team) {
   else alert('Invalid team '+team);
 }
 
-//Re-rolling words
+//Rerolling words
 
-DOM.rerollButton.addEventListener("click",askBoutReroll);
+DOM.rerollButton.addEventListener("click",askAboutReroll);
 
-function askBoutReroll() {
+function askAboutReroll() {
   let ans = prompt(s.ask_for_reroll);
   if(ans && rerollsLeft>0){
     let number = Math.floor(Number(ans));
@@ -282,7 +314,6 @@ function askBoutReroll() {
 
 function reroll(word) {
   let newWord = rerollWordPool[Math.floor(Math.random()*rerollWordPool.length)].toUpperCase();
-  console.log('Rerolled '+words[word-1]+' for '+newWord);
   sendMessage('rerollUsed',{'wordNumber':word, 'newWord':newWord, 'team':team});
 }
 
@@ -295,12 +326,12 @@ function newGame() {
   if (confirm(s.confirm_reset)) {
     var newWordsRed = pickNoDuplicates(wordPool,NUMER_OF_WORDS*2).map(s=>s.toUpperCase());
     var newWordsBlue = newWordsRed.splice(0,NUMER_OF_WORDS);
-    sendMessage('newGame', {'wordsRed':newWordsRed, 'wordsBlue':newWordsBlue});
+    var newStartingTeam = Math.random()<0.5 ? 'R' : 'B';
+    sendMessage('newGame', {'wordsRed':newWordsRed, 'wordsBlue':newWordsBlue, 'startingTeam':newStartingTeam});
   }
 }
 
-function receiveNewGame(newWords) {
-  console.log(newWords);
+function receiveNewGame(data) {
   if(nextTeam !== team){
     team = nextTeam;
     sendMessage('teamSwitch', team);
@@ -314,11 +345,11 @@ function receiveNewGame(newWords) {
   }
   words = [];
   if(team === 'R') {
-    words = newWords.wordsRed;
+    words = data.wordsRed;
     secretWordsDisplay.style.backgroundColor = RED_BACKGROUND;
   }
   if(team === 'B')  {
-     words = newWords.wordsBlue;
+     words = data.wordsBlue;
      secretWordsDisplay.style.backgroundColor = BLUE_BACKGROUND;
   }
   updateWordsDisplay();
@@ -327,6 +358,12 @@ function receiveNewGame(newWords) {
     rerollsLeft = REROLLS_PER_GAME;
     updateRerollButton();  
   }  
+
+  gs.round = 1;
+  gs.startingTeam = data.startingTeam;
+  gs.currentTeam  = data.startingTeam;
+  gs.roundState = RS.START;
+  gs.tokens = {R:{good:0,bad:0}, B:{good:0,bad:0}};
 }
 
 function updateWordsDisplay() {
@@ -360,28 +397,104 @@ function pickNoDuplicates(list,amount,discard) {
   return(res.flat());
 }
 
+//Round progression
+
+function nextState() {
+  gs.roundState++;
+
+  if(round === 1 && gs.roundState === RS.HINT_GIVEN) {
+    gs.roundState++;  //Enemy team doesn't guess in round 1
+  }
+
+  if(gs.roundState === RS.HINT_GIVEN){
+    if(gs.currentTeam === team) addMessageToListDOM(s.time_to_guess_ally);
+    else addMessageToListDOM(s.time_to_guess_ally);
+  }
+
+  if(gs.roundState === RS.ENEMY_GUESSED){
+    if(gs.currentTeam === team) addMessageToListDOM(s.time_to_guess_ally);
+    else addMessageToListDOM(s.time_to_guess_ally);
+  }
+
+  if(team === gs.currentTeam && gs.roundState === RS.ENEMY_GUESSED) addMessageToListDOM(s.time_to_guess_ally);
+  if(team !== gs.currentTeam && gs.roundState === RS.HINT_GIVEN) addMessageToListDOM(s.time_to_guess_enemy);
+
+  if(gs.roundState === RS.ROUND_END){
+    gs.currentTeam = otherTeam(gs.currentTeam);
+    gs.roundState = RS.START;
+    if(gs.startingTeam !== gs.currentTeam){
+      gs.round++;
+      gs.startingTeam = otherTeam(gs.startingTeam);
+      gs.currentTeam = gs.startingTeam;
+      if(gs.round > NUMBER_OF_ROUNDS) endGame('');
+    }
+  }
+}
+
+//Guess processing
+
+function processGuesses(code) {
+  var cteam = gs.currentTeam;
+  var oteam = otherTeam(cTeam);
+  if(guesses[oteam].toString() === code.toString()) gs.tokens[oteam].good++;
+  if(guesses[cteam].toString() !== code.toString()) gs.tokens[cteam].bad++;  
+}
+
+//Ending the game
+function endGame(winningTeam) {
+  gs.roundState = RS.NO_GAME;
+  if(winningTeam === 'R') addMessageToListDOM(s.game_end_red);
+  else if(winningTeam === 'B') addMessageToListDOM(s.game_end_blue);
+  else {
+    var pointDifference = (gs.tokens.R.good - gs.tokens.R.bad) - (gs.tokens.B.good - gs.tokens.B.bad);
+    if(pointDifference === 0) addMessageToListDOM(s.game_end_tie); //TODO further tiebreaker  
+    else if(pointDifference > 0) endGame('R');
+    else if(pointDifference < 0) endGame('B');    
+  }
+}
+
+function checkForTokenVictory() {
+  var blueWin = false;
+  var redWin = false;
+  if(gs.tokens.R.good >= TOKENS_NEEDED) redWin  = true;
+  if(gs.tokens.R.bad >= TOKENS_NEEDED)  blueWin = true;
+  if(gs.tokens.B.good >= TOKENS_NEEDED) blueWin = true;
+  if(gs.tokens.B.bad >= TOKENS_NEEDED)  redWin  = true;
+
+  if(redWin && blueWin) endGame('');
+  else if(redWin) endGame('R');
+  else if(blueWin) endGame('B');
+}
+
 //Sending messages
 DOM.form.addEventListener('submit', sendFormMessage);
 
 function sendFormMessage() {
-  if(code.length === 0 || forceGuessMode){
-    sendMessage('guess', DOM.inputs.map(i=>i.value));
-  } else {
-    sendMessage('hint', DOM.inputs.map(i=>i.value));
-    updateDescriptions(true);
-    forceGuessMode = true;
+  if(code.length === 0 || forceGuessMode){ //Sending a guess
+    if((gs.roundState === RS.ENEMY_GUESSED && gs.currentTeam === team)
+      || (gs.roundState === RS.HINT_GIVEN && gs.currentTeam !== team && gs.round>1)){
+        sendMessage('guess', DOM.inputs.map(i=>i.value));  
+    }else{
+      alert(s.not_your_turn);
+    }
+    
+  } else { //Sending a hint
+    if(gs.roundState = RS.START && gs.currentTeam === team){
+      sendMessage('hint', DOM.inputs.map(i=>i.value));
+      updateDescriptions(true);
+      forceGuessMode = true;  
+      isHintGiver = true;
+    }else{
+      alert(s.not_your_turn);
+    }    
   }
 
   DOM.inputs.forEach(i=>i.value='');  
 }
 
 function sendMessage(type, content) {
-  /*if (content === '') {
-    return;
-  }*/
-  
   drone.publish({
-    room: 'observable-room',
+    room: ROOM_NAME,
     message: {
       type: type,
       content: content
@@ -427,7 +540,7 @@ drone.on('open', error => {
   	members = m;
     if(members.length === 1) {
       //First to enter the room; initialise the game (TODO?)
-      gameState.received = true;
+      gs.received = true;
     }
   	updateMembersDOM(); 
 	});
@@ -436,9 +549,9 @@ drone.on('open', error => {
 	room.on('member_join', member => {
 	  members.push(member);
     addMessageToListDOM(s.joined_game, member); 
-    if(gameState.received){
-      gameState.memberData = members;
-      sendMessage('welcome', gameState);  
+    if(gs.received){
+      gs.memberData = members;
+      sendMessage('welcome', gs);  
     }    
 	  updateMembersDOM();
 	});
@@ -468,9 +581,15 @@ drone.on('open', error => {
             if(i<hints.length-1) res+= ' | ';
           }
           addMessageToListDOM(res, member);
+          gs.hintGiver = member;
+
+          gs.roundState = RS.START; //Reset progress from previous hint if present
+          nextState();
           break;
         case 'guess': //Sent when a player makes a guess
-          addMessageToListDOM(s.sends_guess+': '+data.content, member); 
+          addMessageToListDOM(s.sends_guess+': '+data.content, member); //TODO make sure it's valid
+          guesses[member.team] = guess;
+          nextState();
           break;
         case 'codeDrawn':
           addMessageToListDOM(s.draws_code, member); 
@@ -481,7 +600,9 @@ drone.on('open', error => {
           }
           break;
         case 'codeReveal': //Sent when a secret code is revealed
-          addMessageToListDOM(s.reveals_code+': '+data.content, member); 
+          addMessageToListDOM(s.reveals_code+': '+data.content.code, member); 
+          processGuesses(code);
+          nextState();
           break;
         case 'newGame': //Sent when a new game is started
           addMessageToListDOM(s.started_new_game, member);
@@ -520,9 +641,9 @@ drone.on('open', error => {
           }
           break;
         case 'welcome': //Sent whenever a new player joins the game, informing them of the game state
-          if(!gameState.received){
-            gameState = data.content;
-            let memberData = gameState.memberData;
+          if(!gs.received){
+            gs = data.content;
+            let memberData = gs.memberData;
             for (var i = 0; i < memberData.length; i++) {
               getMember(memberData[i]).team = memberData[i].team;
             }
@@ -540,25 +661,3 @@ drone.on('open', error => {
 });
 
 });
-
-
-/*
-//Load a file, from https://www.codeproject.com/Tips/1165561/How-to-create-a-multilingual-application-using-Jav
-function loadFile(path) {
-  var fileHandle = new XMLHttpRequest();
-  var res;
-  //load content data 
-  fileHandle.open("GET", path, false);
-  fileHandle.onreadystatechange = function () {
-    if(fileHandle.readyState === 4){
-      if(fileHandle.status === 200 || fileHandle.status == 0) {
-        res = fileHandle.responseText;        
-      }
-    }
-  }
-  fileHandle.send();
-  return(res);
-}
-
-console.log(loadFile('en-strings.json'));*/
-
